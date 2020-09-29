a4f6d5f11b061e6eb4eae3e0b5eb81da01ada3a1
user <user@EA08>
2020-09-29 09:40:31 +0800

    2

--- src/learn_github/github.java
+++ src/learn_github/github.java
@@ -4,7 +4,7 @@ public class github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I'm ¼B«³§e");
+		System.out.println("I'm liu-yi-cheng");
 		System.out.println("MY nid is D0808087");
 		System.out.println("This is lab 1 hw");
 	}
